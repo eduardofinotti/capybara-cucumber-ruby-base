423dbdc3cd9ae597d38da05932e1593c1ee6b1ae
barbaracabral <barbaracabral@gmail.com>
2017-11-02 17:58:19 -0200

    Adding last updates

--- features/step_definitions/generic_steps/generic_steps.rb
+++ features/step_definitions/generic_steps/generic_steps.rb
@@ -2,7 +2,7 @@ login_page = LoginPage.new
 
 Given("I have logged in") do
   login_page.load
-  login_page.username_login.set 'barbaracabral@gmail.com'
+  login_page.username_login.set 'gbopoeta@gmail.com'
   login_page.password_login.set 'fr$$d0m83'
   login_page.loginSubmit.click
 end
